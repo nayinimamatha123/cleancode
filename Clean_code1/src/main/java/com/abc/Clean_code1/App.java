823ec64b6e5b1886a314f1bed38c33c5cc94f363
Mamatha Nayini <Mamatha Nayini@LAPTOP-D4DGKQ8H>
2020-07-20 09:29:23 +0530

    first commit

---
+++ Clean_code1/src/main/java/com/abc/Clean_code1/App.java
@@ -0,0 +1,16 @@
+package com.abc.Clean_code1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	 Construction construction = new Construction();
+	        construction.takeInput();
+	        construction.calculateAndDisplayTotalCost();
+	    }
+    }
+
